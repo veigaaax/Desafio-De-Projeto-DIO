fef3639fc9ffe4b240b28086d26019987bccd780
veigaaax <jovanav65@hotmail.com>
2024-06-05 19:02:52 -0300

    Projeto

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("ola pessoas")
+let nomedoheroi = "jovana";
+let exp = 0;
+let classe= "";
+
+
+while (exp < 10000){
+
+if ( exp < 1000){
+  classe = "ferro";
+    
+} else if (exp >= 1001 && exp <= 2000){
+    classe = "bronze";
+
+} else if (exp >= 2001 && exp <= 5000){
+    classe = "prata";
+
+} else if (exp >= 5001 && exp <= 7000){
+    classe = "ouro";
+
+} else if (exp >= 7001 && exp <= 8000){
+    classe = "Platina";
+
+} else if (exp >= 8001 && exp <= 9000){
+    classe = "Ascendente";
+
+} else if (exp >= 9001 && exp <= 10000){
+    classe = "Imortal";
+
+} else if (exp >=10001){
+    classe = "Radiante";
+   
+}
+
+exp += 500;
+}
+
+console.log("O Herói com o nome de  " + nomedoheroi + " está no nível de " + classe);
